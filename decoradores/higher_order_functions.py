fb352e60824fba8acb262e30525422394d676755
Victor Hugo Nascimento Oliveira <victor.oliveira@olist.com>
2021-09-10 14:16:44 -0300

    adding higher_order_functions

--- decoradores/higher_order_functions.py
+++ decoradores/higher_order_functions.py
@@ -3,6 +3,8 @@
  - ou ate mesmo podemos passar funcoes como argumentos para outras funcoes,
  - e ate mesmo criar variaveis do tipo funcoes
 
+ First Class Citizen
+
 """
 
 
@@ -26,4 +28,40 @@ def calculate(num1, num2, fuction):
     return fuction(num1, num2)
 
 
-print(calculate(4, 3, multiply))
+# Nested Functions - Funcoes aninhadas
+
+
+#   Funcoes dentro de funcoes, conhecida por Nested Functions or Innter Functions (Funcoes Internas)
+
+
+from random import choice
+
+
+def greeting(people):
+    def humor():
+        return choice(('E ai ', 'Suma daqui ', 'Gosto muito de voce '))
+
+    return humor() + people
+
+
+# return function  of others function
+
+def make_me_laugh():
+    def to_laugh():
+        return choice(('ahahahahahaha', 'asahsuahsuahsa', 'uauauauaush'))
+
+    return to_laugh()
+
+
+# Inner Functions (Funcoes Internas / Nested Functions) Podem acessar o escopo de funcoes mais externas.
+
+def make_me_laugh_again(people):
+    def laughing():
+        r = choice(('ahsauhuahusa', 'hahaahahah', 'k'))
+        return f'{r} {people}'
+
+    return laughing()
+
+
+people = make_me_laugh_again('victor')
+print(people)
